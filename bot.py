4123306a7eb4154917db74eb5d1dac4a55c0b472
Ethan Crann <ecrann@protonmail.com>
2020-05-13 09:47:54 -0300

    Added option to choose either daily or intraday data

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import pandas as pd
 import datetime as dt
 import mplfinance as mpf
 import numpy as np
+import time as time
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.cryptocurrencies import CryptoCurrencies
 from alpha_vantage.techindicators import TechIndicators
@@ -51,15 +52,33 @@ async def on_message(message):
     if args[1] == "stock":
         CUR = "CAD"
         ts = TimeSeries(key=ALPHA_TOKEN, output_format='pandas')
-        data, meta_data = ts.get_intraday(symbol=args[2],interval='1min', outputsize='full')
-        data.rename(index= pd.to_datetime,columns = {"1. open":"Open","2. high":"High","3. low":"Low","4. close":"Close","5. volume":"Volume"},inplace = True)
-        data = data.replace("",np.nan)
-        data.index=pd.to_datetime(data.index)
-        data = data.iloc[::-1]
-        data.resample("1 min")
-        plt.title("Testing")
-        mpf.plot(data,type="line",savefig="graph.png")
+        
+        if args[3] == "intraday":
+            try:
+                data, meta_data = ts.get_intraday(symbol=args[2],interval='1min', outputsize='full')
+                print(data)
+                data.rename(index= pd.to_datetime,columns = {"1. open":"Open","2. high":"High","3. low":"Low","4. close":"Close","5. volume":"Volume"},inplace = True)
+                data = data.replace("",np.nan)
+                data.index=pd.to_datetime(data.index)
+                data = data.iloc[::-1]
+            except ValueError as err:
+                await message.channel.send("Invalid Ticker")
+
+        if args[3] == "daily":
+            try:
+                data, meta_data = ts.get_daily(symbol=args[2],outputsize='full')
+                print(data)
+                data.rename(index= pd.to_datetime,columns = {"1. open":"Open","2. high":"High","3. low":"Low","4. close":"Close","5. volume":"Volume"},inplace = True)
+                data = data.replace("",np.nan)
+                data.index=pd.to_datetime(data.index)
+                data = data.iloc[::-1]
+            except ValueError as err:
+                await message.channel.send("Invalid Ticker")
+        
+        print(data)
+        mpf.plot(data,type="line",savefig="graph.png",title="Price chart for "+args[2])
         print("Finished stock plot creation")
+
         await message.channel.send(file=discord.File('graph.png'))
 
 client.run(DISCORD_TOKEN)
